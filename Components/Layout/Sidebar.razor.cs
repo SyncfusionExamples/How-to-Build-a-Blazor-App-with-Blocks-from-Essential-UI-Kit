3e4b9f6ba216af874afe9291a00a44036720d682
Alan <alans@syncfusion.com>
2025-04-10 18:27:24 +0530

    00000: Added Support portal demo

---
+++ Components/Layout/Sidebar.razor.cs
@@ -0,0 +1,49 @@
+using Microsoft.AspNetCore.Components;
+using Syncfusion.Blazor.Lists;
+
+namespace UI_Kit_Sample.Components.Layout
+{
+    public partial class Sidebar
+    {
+        [Inject]
+        private NavigationManager NavigationManager { get; set; }
+
+        private readonly List<SidebarMenuItem> _data = new List<SidebarMenuItem>
+        {
+            new SidebarMenuItem { Id = 1, Title = "My Dashboard", FontIcon = "e-icons e-home", Path = "home" },
+            new SidebarMenuItem { Id = 2, Title = "All Tickets", FontIcon = "e-icons e-notes", Path = "tickets" },
+            new SidebarMenuItem { Id = 3, Title = "Chat", FontIcon = "e-icons e-comment-show", Path = "chat" },
+            new SidebarMenuItem { Id = 4, Title = "Contacts", FontIcon = "e-icons e-user", Path = "contacts" },
+            new SidebarMenuItem { Id = 5, Title = "Settings", FontIcon = "e-icons e-settings", Path = "settings" }
+        };
+        public List<SidebarMenuItem> ListData { get; set; } = new List<SidebarMenuItem>();
+
+        protected override void OnInitialized()
+        {
+            int index = _data.FindIndex(data => NavigationManager.Uri.Contains(data.Path));
+            var tempData = _data;
+            foreach (var item in tempData)
+            {
+                item.SelectedItem = new Dictionary<string, object>();
+            }
+            if (index >= 0 && index < tempData.Count)
+            {
+                tempData[index].SelectedItem = new Dictionary<string, object> { { "class", "e-active" } };
+            }
+            ListData = tempData;
+        }
+        public void OnClickSidebarItem(ClickEventArgs<SidebarMenuItem> args)
+        {
+            NavigationManager.NavigateTo("support/" + args.ItemData.Path);
+        }
+    }
+
+    public class SidebarMenuItem
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string FontIcon { get; set; } = string.Empty;
+        public string Path { get; set; } = string.Empty;
+        public Dictionary<string, object> SelectedItem { get; set; } = new Dictionary<string, object>();
+    }
+}
